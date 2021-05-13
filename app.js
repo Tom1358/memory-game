174f8d53c155096e73ce8b0c8993f9f7c16c5faf
Tom1358 <thomasellis358@hotmail.com>
2021-05-13 19:47:43 +0000

    Add three more fruit to make 12 in total to fill 3 x 4 game board.  Also changed 'evil' figure to 'skeleton'

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const cardArray = [
         img: '/assets/images/cherries.jpg'
     },
     {
+        name: 'dragon_fruit',
+        img: '/assets/images/dragon_fruit.jpg'
+    },
+    {
         name: 'grapes',
         img: '/assets/images/grapes.jpg'
     },
@@ -32,8 +36,16 @@ const cardArray = [
         img: '/assets/images/pineapple.jpg'
     },
     {
+        name: 'star_fruit',
+        img: '/assets/images/star_fruit.jpg'
+    },
+    {
         name: 'strawberry',
         img: '/assets/images/strawberry.jpg'
+    },
+    {
+        name: 'tomato',
+        img: '/assets/images/tomato.jpg'
     },
     {
         name: 'watermelon',
@@ -41,17 +53,28 @@ const cardArray = [
     }
 ]
 
-const grid = document.getElementsByClassName('grid') // or could have used document.querySelector('.grid')
+const grid = document.getElementsByClassName('grid'); // or could have used document.querySelector('.grid')
+const cardsChosen = [];
 
 // create the board
 function createBoard() {
     for (let i=0; i < cardArray.length; i++) {
-        let card = document.createElement('img')
-        card.setAttribute('src', '/assets/images/wizard.png')
-        card.setAttribute('data-id', i)
-        //card.addEventListener('click', flipCard)
-        grid.appendChild(card)
+        var card = document.createElement('img');
+        card.setAttribute('src', '/assets/images/skeleton.png');
+        card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard)
+        grid[0].appendChild(card);
+       
     }
+};
+
+// check for matches
+
+// flip your card
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
 }
 
 createBoard();
